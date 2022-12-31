e16ad7b90a9315ab7e21dcf086d37a95d246efd7
Adam Gariba <agariba101@gmail.com>
2022-12-31 16:55:50 -0700

    Basic implementation of home view

--- homebase/home/routes.py
+++ homebase/home/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import requests
 
 home_bp = Blueprint('home_bp', __name__)
 
-@home_bp.route('/')
+@home_bp.route("/")
 def index():
-    return "Homepage"
+    try:
+        response = requests.get("https://statsapi.mlb.com/api/v1/standings?leagueId=103,104")
+        response.raise_for_status()
+
+        results = response.json()
+        return render_template("home/index.html", records=results['records']) 
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
