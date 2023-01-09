251b257d12919d3811083ebbf01895bc647f5e15
Adam Gariba <agariba101@gmail.com>
2023-01-08 22:10:36 -0500

    Player page styles

--- homebase/static/js/main.js
+++ homebase/static/js/main.js
@@ -3,6 +3,14 @@ var pitchers_field = document.getElementById('pitchers')
 var hitters_table = document.getElementById('hitters_tbl')
 var pitchers_table = document.getElementById('pitchers_tbl')
 
+// Set the Hitters table to be shown on default
+window.onload = (event) => {
+    hitters_field.checked = true;
+    hitters_table.style.display = "table";
+    pitchers_table.style.display = "none";
+};
+
+// Toggles between the hitters and pitchers table
 function showTable() {
     if(hitters_field.checked) {
         hitters_table.style.display = "table";
