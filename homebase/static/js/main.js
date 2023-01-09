2abeaf667bb60a426ea639ab7190d27b99faff15
Adam Gariba <agariba101@gmail.com>
2023-01-08 20:23:13 -0500

    Team page styled

--- homebase/static/js/main.js
+++ homebase/static/js/main.js
@@ -5,11 +5,11 @@ var pitchers_table = document.getElementById('pitchers_tbl')
 
 function showTable() {
     if(hitters_field.checked) {
-        hitters_table.style.display = "block";
+        hitters_table.style.display = "table";
         pitchers_table.style.display = "none";
     }
     else if(pitchers_field.checked) {
-        pitchers_table.style.display = "block";
+        pitchers_table.style.display = "table";
         hitters_table.style.display = "none";
     }
 }
